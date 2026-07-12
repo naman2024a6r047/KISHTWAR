95d4916c6027c016eb5fc8549263d47ad43182f3
naman2024a6r047 <naman0503o@gmail.com>
2026-07-12 08:49:57 +0530

    update: migration schema

--- prisma/migrations/20260613192928_init_schema/migration.sql
+++ prisma/migrations/20260613192928_init_schema/migration.sql
@@ -754,3 +754,4 @@ ALTER TABLE `activity_logs` ADD CONSTRAINT `activity_logs_user_id_fkey` FOREIGN 
 
 -- AddForeignKey
 ALTER TABLE `page_views` ADD CONSTRAINT `page_views_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ 
